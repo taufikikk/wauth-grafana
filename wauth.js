8a1941caea091cd1ba9ffdc15d99e396a9ceb8b0
IT E-Channel <kebhanaindonesia@gmail.com>
2022-12-28 11:13:50 +0700

    Initial commit

---
+++ wauth.js
@@ -0,0 +1,25 @@
+const express = require('express'),
+      bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const jwt = require('jsonwebtoken')
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/wauth', function(req, res) {
+  res.sendFile(__dirname + '/form.html');
+});
+
+app.post('/wauth', function(req, res) {
+  console.log(req.body);
+  let msg = req.body
+  let groups = msg.group.split(',')
+  console.log(groups);
+  let generateToken = jwt.sign(req.body, 'nyan');
+  res.send(JSON.stringify(generateToken))
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
